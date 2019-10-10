0ebaa265ee4e55fb180fd012c070284b9aa569c5
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-10-10 10:44:50 -0400

    added spotify ID ad Secret to keys.js

--- keys.js
+++ keys.js
@@ -2,6 +2,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.ac414f73a00f405890dbcbd292c0f5a8,
+  secret: process.env.7e2a03e8cde0423f8b4b5de41631863a
 };
