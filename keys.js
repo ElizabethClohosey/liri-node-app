ad93261647fef77dc2642ec21a1e13145598a627
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-10-13 19:58:57 -0400

    all keys are console logging

--- keys.js
+++ keys.js
@@ -1,6 +1,14 @@
-console.log('this is loaded');
+console.log('keys.js is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+  key: process.env.OMDb_KEY,
+};
+
+exports.bandsInTown = {
+  key: process.env.Bandsintown_API,
 };
--- liri.js
+++ liri.js
@@ -2,6 +2,13 @@
   require("dotenv").config();
 
   var keys = require("./keys.js");
+  // console.log("These are all my keys", keys);
 
-  var spotify = new Spotify(keys.spotify);  
+  // imports the node-spotify-api NPM package
+  var Spotify = require("node-spotify-api");
+
+  // initializes the spotify API client using client id and secret
+  var spotify = new Spotify(keys.spotify)  
   console.log("Spotify keys", spotify);
+
+ 
