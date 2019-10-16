d990302e47568886f00155b7d4b28eb39e4d5278
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2019-10-15 22:19:09 -0400

    OMDB, bands in town and partial-spotify working with commands

--- liri.js
+++ liri.js
@@ -12,10 +12,18 @@
   // console.log("Spotify keys", spotify);
 
   var axios = require("axios");
-  var userInput = process.argv[2];
+  var bandsintown = require("bandsintown-rest");
 
-// Then run a request with axios to the OMDB API with the movie specified
-axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=defd7e02").then(
+  var command = process.argv[2];
+  var userInput = process.argv[3];
+
+
+// Request with axios to the OMDB API with the movie specified
+switch (command) {
+
+  case "movie-this":
+
+axios.get("http://www.omdbapi.com/?t=" + userInput + ";&y=&plot=short&apikey=defd7e02").then(
   function(response) {
     console.log("The movie's name is: " + response.data.Title);
     console.log("The movie's release year is: " + response.data.Year);
@@ -29,35 +37,56 @@ axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=defd
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-  }
-
-
-  function helpMe (songName) { 
-    // console.log(userInput, "user Input")
-  // spotify search (find this in spotify docs)
-  spotify.search({ 
-    type: 'track', 
-    query: userInput, 
-    limit: 3,
-  }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
     }
-  
-    // console.log(data); 
-    data.tracks.items.map(item => {
-      // console.log(item)
-      // console.log(item.album.artists.external_urls.name);
-      // console.log(item.album);
+    break;
 
-      // These console logs work 
-      console.log(item.name);
-      console.log(item.external_urls);
+    case "concert-this":
 
-    })
-  });
-}  
-helpMe();
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
+      function(response) {
+        for (let i = 0; i < 4; i++) {
+          console.log("The name of venue is " + response.data[i].venue.name);
+          console.log("The venue location is " + response.data[i].venue.city);
+          console.log("The date of event " + response.data[i].venue.datetime);
+        }
+      }), function(err, data) {
+          if (err) {
+            return console.log('Error occurred: ' + err);
+          }
+      }
+    break;
+
+    case "spotify-this-song":
+
+        function helpMe (songName) { 
+          // console.log(userInput, "user Input")
+        // spotify search (find this in spotify docs)
+        spotify.search({ 
+          type: 'track', 
+          query: userInput, 
+          limit: 3,
+        }, function(err, data) {
+          if (err) {
+            return console.log('Error occurred: ' + err);
+          }
+        
+          // console.log(data); 
+          data.tracks.items.map(item => {
+            // console.log(item)
+            // console.log(item.album.artists.external_urls.name);
+            // console.log(item.album);
+      
+            // These console logs work 
+            console.log(item.name);
+            console.log(item.external_urls);
+      
+          })
+        });
+      }  
+      helpMe();
+
+      break;
+}
 
 
 
@@ -66,15 +95,11 @@ helpMe();
 
 
 
-                        // Saved code for possible later use 
 
-  // axios.get("http://www.bandsintown.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
-  // function(response) {
-  //   console.log("The event data is: " + response.data.eventData);
-  // }), function(err, data) {
-  //     if (err) {
-  //       return console.log('Error occurred: ' + err);
-  //     }
-  // }
+
+
+            // Saved code for possible later use 
+                        
+
   
 
